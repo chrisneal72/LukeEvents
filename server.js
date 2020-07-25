6bac2c603f28459cfafbdec57857a5d913c2e823
Chris Neal <voodoodaddy2000@gmail.com>
2020-07-25 01:00:32 -0700

    Trying for Heroku again

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const path = require("path");
 
-app.use(express.static("build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.get("*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "build/index.html"));
