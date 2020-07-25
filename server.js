23375fe9cc395825c375b16afd81421ac7d39fe5
Chris Neal <voodoodaddy2000@gmail.com>
2020-07-25 06:53:00 -0700

    Trying for Heroku again 5

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure body parsing for AJAX requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("build"));
 
-// Add routes, both API and view
-app.use(routes);
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, () =>
