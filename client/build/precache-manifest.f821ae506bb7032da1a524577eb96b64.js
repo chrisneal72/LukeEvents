5df957da1333f16dd13655121d1c6af9ac82322a
Chris Neal <voodoodaddy2000@gmail.com>
2020-07-31 21:24:49 -0700

    Run Build for Heroku

---
+++ client/build/precache-manifest.f821ae506bb7032da1a524577eb96b64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4033ceda27b14020e383a16a8a45779d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b57a89a2964a56e08dd7",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "a60c3aab49ea9a27f709",
+    "url": "/static/css/main.06ade20b.chunk.css"
+  },
+  {
+    "revision": "b57a89a2964a56e08dd7",
+    "url": "/static/js/2.4a2b9981.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.4a2b9981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60c3aab49ea9a27f709",
+    "url": "/static/js/main.4db68dd4.chunk.js"
+  },
+  {
+    "revision": "d51ffc911fc4482226c3",
+    "url": "/static/js/runtime-main.ae590015.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
