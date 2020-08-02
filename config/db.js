03fd081169991f5758fcc930cf3914c894a2f7fb
Chris Neal <voodoodaddy2000@gmail.com>
2020-08-01 17:07:48 -0700

    Trying to get the static IP working on Heroku

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-
+const proxy = process.env.QUOTAGUARDSTATIC_URL;
 const connectToDatabase = async () => {
   const connection = await mongoose.connect(
-    process.env.MONGODB_CLOUD_URI || "mongodb://localhost/lukeevents",
+    // process.env.MONGODB_CLOUD_URI || "mongodb://localhost/lukeevents",
+    process.env.MONGODB_CLOUD_URI,
     {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      proxy: proxy
     }
   );
 
--- models/page.js
+++ models/page.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 const pageSchema = new mongoose.Schema(
   {
     template: {
